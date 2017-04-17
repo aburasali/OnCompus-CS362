987c62e3e63ac8fdf36f47fc533ccfc623f5437c
aburasali <aburasali@gmail.com>
2017-04-17 13:15:17 -0700

    add calendar application in Asssignment-2 folder

---
+++ projects/aburasa/assignment-2/Calendar/src/test/java/edu/osu/cs362/CalDayTest.java
@@ -0,0 +1,19 @@
+package edu.osu.cs362;
+/**
+ *  This class provides a basic set of test cases for the
+ *  CalDay class.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalDayTest {
+    /**
+     * Test that the gets methods work as expected.
+     */
+	 @Test
+	  public void test01()  throws Throwable  {
+
+	 }
+	
+}
---
+++ projects/aburasa/assignment-2/Calendar/src/test/java/edu/osu/cs362/TimeTableTest.java
@@ -0,0 +1,19 @@
+package edu.osu.cs362;
+/**
+ *  This class provides a basic set of test cases for the
+ *  TimeTable class.
+ */
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TimeTableTest {
+   	 @Test
+	  public void test01()  throws Throwable  {
+	
+		 
+   		
+	 }
+
+}
